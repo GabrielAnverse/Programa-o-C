ee4ccc5a5d32eb85224b2a7ed9466946ea5d10c3
GabrielAnverse <gabrielmacielcpt@gmail.com>
2025-02-11 16:48:53 +0000

    while

---
+++ 4_Estruturas_Repetição/Do_While.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+
+    /*
+    int i = 1;
+   
+    do { //Do = Faça
+        printf("%d\n", i);
+        i++;
+    } while (i <= 5);
+    */
+
+   int i = 1
+
+
+   
+    return 0;
+}
---
+++ 4_Estruturas_Repetição/While.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int i = 1;
+    //while = enquanto
+    while (i <= 5) { //Faz a contagem de 1 a 5, I vai ser printado e acrescentar +1 até ser maior que 5
+        printf("%d\n", i);
+        i++;
+    }
+   
+    return 0;
+}
