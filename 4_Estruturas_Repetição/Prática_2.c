027eb19a33d31c32ca5e2a0cb7604728f62b91e0
GabrielAnverse <gabrielmacielcpt@gmail.com>
2025-02-14 16:23:48 +0000

    começar vetores

--- 4_Estruturas_Repetição/Prática_2.c
+++ 4_Estruturas_Repetição/Prática_2.c
@@ -2,23 +2,19 @@
 
 int main() {
 
+    //Pratico de loops avançados, for dentro de for.
+    //Toda vez que for rodar o primeiro loop(de fora) ele vai rodar o loop de dentro todas as vezes
     int i, j;
-
     char letra;
 
-    for (i = 1; i <=5; i++)
-    {
+    for (i = 1; i <=5; i++){
         letra = 'A';
-
-        for(j = 1; j <= i; j++)
-        {
+        for(j = 1; j <= i; j++){
             printf("%c ", letra);
             letra++;
         }
         printf("\n");
-    }
-    
-
+    }   
 
     return 0;
 }
